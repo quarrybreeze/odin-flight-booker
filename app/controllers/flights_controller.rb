69e2b698bd7d7513600c602e4b388829b1628b74
Terrence Clayton <184171442+quarrybreeze@users.noreply.github.com>
2025-04-02 13:17:29 -0700

    New: added feature to choose flight from search results via radio button, added some basic styling for flight results.

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -10,13 +10,14 @@ class FlightsController < ApplicationController
         departure_airport_id: params[:departure_airport_id],
         arrival_airport_id: params[:arrival_airport_id]
       ).where(start: date.beginning_of_day..date.end_of_day) if date
+      @num_tickets = params[:num_tickets]
     else
-      @search_results = Flight.none
+      # @search_results = Flight.none
     end
   end
 
   private
   def search_params
-    params.permit(:departure_airport_id, :arrival_airport_id, :start, :num_tickets, :commit)
+    params(:search).permit(:departure_airport_id, :arrival_airport_id, :start, :num_tickets, :commit)
   end
 end
